82cf860f0c61b90f75996fda8ceac9cf1708fdba
Joah Gerstenberg <joahd@gbergfamily.com>
2012-11-17 17:11:43 -0600

    Started working…..

---
+++ js/script.js
@@ -0,0 +1,49 @@
+var b = 0
+function s(n) {
+   var i;
+   if (typeof n === "string") {
+      if (n.length > 1) {
+         a = [];
+         for (i in n) {
+            a.push(s(n[i]));
+         }
+         return a;
+      } else {
+         return n.charCodeAt(0);
+      }
+   } else {
+      if (typeof n === "object"){
+         a = [];
+         for (i in n) {
+            a.push(s(n[i]));
+         }
+         return a;
+      }
+      return String.fromCharCode(n);
+   }
+}
+
+function span(w) {
+    a = [];
+    for (i in w){
+        a.push("<span id='"+i+"'>"+w[i]+"</span>");
+    }
+    return a;
+}
+
+function newBranch() {
+    w = "";
+    $("#container").append("<div id='branch' class='b"+b.toString(10)+"'>"+w+"</div>")
+    b += 1
+}
+
+$(document).ready(function() {
+    function start() {
+        newBranch()
+        while (parseInt($("#branch.b"+(b-1).toString(10)).css("right"),10) === -100) {
+          setInterval(function() {
+            $("#branch.b"+(b-1).toString(10)).css("right","+=1")
+        }, 50)}
+    }
+    start()
+});
